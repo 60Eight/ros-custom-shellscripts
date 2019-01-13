1d4955357aa9b9d05736197048e6e4f8a54cd570
60Eight <23185944+60eight@users.noreply.github.com>
2019-01-14 08:49:25 +1100

    Remove renaming of the libjni_latinimegoogle.so

--- 00002-install-swipe-library.sh
+++ 00002-install-swipe-library.sh
@@ -4,8 +4,8 @@ IME_LIBRARY="https://github.com/opengapps/arm64/raw/master/lib64/28/libjni_latin
 KB_DECODER_LIBRARY="https://github.com/opengapps/arm64/raw/master/lib64/23/libjni_keyboarddecoder.so"
 mkdir -p "${BUILD_DIR}/custom"
 echo "Downloading swipe library (IME) ${IME_LIBRARY}"
-wget -O "${BUILD_DIR}/custom/libjni_latinime.so" "${IME_LIBRARY}"
+wget -O "${BUILD_DIR}/custom/libjni_latinimegoogle.so" "${IME_LIBRARY}"
 echo "Downloading swipe library (KB_DECODER) ${KB_DECODER_LIBRARY}"
 wget -O "${BUILD_DIR}/custom/libjni_keyboarddecoder.so" "${KB_DECODER_LIBRARY}"
-sed -i "\$aPRODUCT_COPY_FILES += custom/libjni_latinime.so:system/lib64/libjni_latinime.so" ${BUILD_DIR}/build/make/target/product/core.mk
+sed -i "\$aPRODUCT_COPY_FILES += custom/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so" ${BUILD_DIR}/build/make/target/product/core.mk
 sed -i "\$aPRODUCT_COPY_FILES += custom/libjni_keyboarddecoder.so:system/lib64/libjni_keyboarddecoder.so" ${BUILD_DIR}/build/make/target/product/core.mk
