963c7badeee40ae4e3cb0cf8215913c60c25e4c4
60Eight <23185944+60eight@users.noreply.github.com>
2019-01-14 01:02:29 +1100

    Add libjni_keyboarddecoder.so for keyboard swipe gesture to work

--- 00002-install-swipe-library.sh
+++ 00002-install-swipe-library.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-SWIPE_LIBRARY="https://github.com/opengapps/arm64/raw/master/lib64/28/libjni_latinimegoogle.so"
+IME_LIBRARY="https://github.com/opengapps/arm64/raw/master/lib64/28/libjni_latinimegoogle.so"
+KB_DECODER_LIBRARY="https://github.com/opengapps/arm64/raw/master/lib64/23/libjni_keyboarddecoder.so"
 mkdir -p "${BUILD_DIR}/custom"
-echo "Downloading swipe library ${SWIPE_LIBRARY}"
-wget -O "${BUILD_DIR}/custom/libjni_latinime.so" "${SWIPE_LIBRARY}"
+echo "Downloading swipe library (IME) ${IME_LIBRARY}"
+wget -O "${BUILD_DIR}/custom/libjni_latinime.so" "${IME_LIBRARY}"
+echo "Downloading swipe library (KB_DECODER) ${KB_DECODER_LIBRARY}"
+wget -O "${BUILD_DIR}/custom/libjni_keyboarddecoder.so" "${KB_DECODER_LIBRARY}"
 sed -i "\$aPRODUCT_COPY_FILES += custom/libjni_latinime.so:system/lib64/libjni_latinime.so" ${BUILD_DIR}/build/make/target/product/core.mk
+sed -i "\$aPRODUCT_COPY_FILES += custom/libjni_keyboarddecoder.so:system/lib64/libjni_keyboarddecoder.so" ${BUILD_DIR}/build/make/target/product/core.mk
